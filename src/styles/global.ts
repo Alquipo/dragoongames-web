06ec52f8613486f19ddefb05dc832b69b0804541
Alquipo <alquiponeto@outlook.com.br>
2020-12-27 00:28:51 -0300

    chore: fonts Poppins

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,43 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+}
   *{
     margin: 0;
     padding: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html{
-    font-size: 62.5%large;
+    font-size: 62.5%;
   }
 
   html, body, #__next{
@@ -16,7 +45,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body{
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 
   a {
