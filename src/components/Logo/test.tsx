07cd833eeb1822b634099fb6a9bc59b925bc5e4e
Alquipo <alquiponeto@outlook.com.br>
2020-12-27 01:48:41 -0300

    chore: heper themeProvider

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,11 +3,9 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a white label by default', () => {
+    //renderizar o componente `render`
+    //selecionar o elemento a ser testado `screen` (queries) - getByLabel
+    //expect - assertion- asserção -comparação- analise (espero que renderize a logo branca)
   })
 })
