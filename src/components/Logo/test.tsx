63ead47cb7d57ea02953d66997caf47aaf4f8dcf
Alquipo <alquiponeto@outlook.com.br>
2020-12-27 02:06:38 -0300

    chore: create test Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,23 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
+//renderizar o componente `render`
+//selecionar o elemento a ser testado `screen` (queries) - getByLabel
+//expect - assertion- asserção -comparação- analise (espero que renderize a logo branca)
 
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
-    //renderizar o componente `render`
-    //selecionar o elemento a ser testado `screen` (queries) - getByLabel
-    //expect - assertion- asserção -comparação- analise (espero que renderize a logo branca)
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
