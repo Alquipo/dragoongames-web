f09d1d43dfd3dc72d72d35dd33343de926eeb75e
Alquipo <alquiponeto@outlook.com.br>
2020-12-27 02:30:35 -0300

    feat: Logo component complete

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,4 +1,6 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
 import { LogoProps } from '.'
 
 const wrapperModifiers = {
@@ -10,13 +12,31 @@ const wrapperModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+    width: 5.8rem;
+    height: 4.5rem;
+
+    svg{
+      height: 4.5rem;
+      pointer-events: none;
+    }
+
+    .text{
+      display: none;
+    }
+  `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
     ${!!size && wrapperModifiers[size]}
+
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 //renderizar o componente `render`
@@ -28,10 +29,17 @@ describe('<Logo />', () => {
     })
   })
 
-  it('should render a big logo', () => {
+  it('should render a bigger logo', () => {
     renderWithTheme(<Logo size="large" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(
+      screen.getByLabelText(/Won Games/i).parentElement
+    ).toHaveStyleRule('width', '5.8rem', { media: '(max-width: 768px)' })
   })
 })
