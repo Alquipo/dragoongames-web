814121630f5b6b8d306514279fda8436578b68a4
Alquipo <alquiponeto@outlook.com.br>
2020-12-27 01:02:09 -0300

    fix: path storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,9 @@ module.exports = {
     '../src/components/**/stories.mdx',
     '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-essentials']
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
