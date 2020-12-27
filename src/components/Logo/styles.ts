b4abcba6d63ca8bf71643252f123215558d655f5
Alquipo <alquiponeto@outlook.com.br>
2020-12-27 01:38:28 -0300

    fix: logo svg

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,3 +1,8 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { LogoProps } from '.'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, color }) => css`
+    color: ${theme.colors[color!]};
+  `}
+`
