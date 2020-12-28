57738b821674bdf82e48ff6bfc27ecc70035a6db
Alquipo <alquiponeto@outlook.com.br>
2020-12-28 00:21:52 -0300

    finished component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,89 @@
+import Link from 'next/link'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact Us
+        </Heading>
+
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+        <a href="tel:+55229999999999">+55 22 9999999999</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Follow us
+        </Heading>
+
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Links
+        </Heading>
+
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column aria-labelledby="footer-contact">
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Location
+        </Heading>
+        <span>Rua 7 de Maio</span>
+        <span>527 - 89020330</span>
+        <span>Rio de Janeiro, Brasil</span>
+      </S.Column>
+    </S.Content>
+
+    <S.Copyright>Won Games 2020 © All rights reserved.</S.Copyright>
+  </S.Wrapper>
+)
+
+export default Footer
