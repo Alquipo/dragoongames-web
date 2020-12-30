e093ebcb08374b88d6eaa1d1af0048f45c27929f
Alquipo <alquiponeto@outlook.com.br>
2020-12-30 03:02:12 -0300

    fix: test Gamecard component

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,4 +1,8 @@
-import { AddShoppingCart, FavoriteBorder } from 'styled-icons/material-outlined'
+import {
+  AddShoppingCart,
+  Favorite,
+  FavoriteBorder
+} from 'styled-icons/material-outlined'
 
 import Button from 'components/Button'
 import * as S from './styles'
@@ -9,13 +13,17 @@ export type GameCardProps = {
   img: string
   price: string
   promotionalPrice?: string
+  favorite?: boolean
+  onFav?: () => void
 }
 const GameCard = ({
   title,
   developer,
   img,
   price,
-  promotionalPrice
+  promotionalPrice,
+  favorite = false,
+  onFav
 }: GameCardProps) => (
   <S.Wrapper>
     <S.ImageBox>
@@ -26,8 +34,12 @@ const GameCard = ({
         <S.Title>{title}</S.Title>
         <S.Developer>{developer}</S.Developer>
       </S.Info>
-      <S.FavButton role="button">
-        <FavoriteBorder aria-label="Add to Wishlist" />
+      <S.FavButton onClick={onFav} role="button">
+        {favorite ? (
+          <Favorite aria-label="Remove from Wishlist" />
+        ) : (
+          <FavoriteBorder aria-label="Add to Wishlist" />
+        )}
       </S.FavButton>
       <S.BuyBox>
         {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
