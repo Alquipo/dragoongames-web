765424bf659ce68f4851beddfcbe02dae35d1c0a
Alquipo <alquiponeto@outlook.com.br>
2020-12-30 02:46:29 -0300

    fix: GameCard component

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -8,7 +8,13 @@ export default {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     img: '/img/red-dead-img.jpg',
-    price: 'R$ 235,00'
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
   }
 } as Meta
 
