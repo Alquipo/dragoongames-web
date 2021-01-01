a13f8b7f0ddf9f0e51deb241d590b355c867753d
Alquipo <alquiponeto@outlook.com.br>
2021-01-01 19:39:03 -0300

    optimizer Home test

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -10,29 +10,32 @@ import Home from '.'
 
 const props = {
   banners: bannerMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcomingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcomingGames: [gamesMock[0]],
   upcomingHighlight: highlightMock,
-  upcomingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcomingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighlight: highlightMock
 }
 
 describe('<Home />', () => {
-  it('should render menu and footer', () => {
+  it('should render all components Home Page', () => {
     renderWithTheme(<Home {...props} />)
 
+    //menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    //footer
     expect(
       screen.getByRole('heading', { name: /follow us/i })
     ).toBeInTheDocument()
-    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
-  })
 
-  it('should render sections', () => {
-    renderWithTheme(<Home {...props} />)
+    //logos (menu/footer)
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    //sections
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /most popular/i })
@@ -43,14 +46,11 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render section elements', () => {
-    renderWithTheme(<Home {...props} />)
     // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+    // card game ( 5 sections com 4 cards cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
     // highlight
     expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
