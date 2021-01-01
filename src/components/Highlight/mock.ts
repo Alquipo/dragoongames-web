1a090dd3d05088b0377fc0e5b4701c5929ed2e4e
Alquipo <alquiponeto@outlook.com.br>
2021-01-01 18:54:25 -0300

    chore: mock data

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Read Dead is back!',
+  subtitle: "come see John's new adventures",
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLabel: 'buy now',
+  buttonLink: '/games/rdr2'
+}
