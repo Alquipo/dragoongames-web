db3542ee1848b33f5ecda3b9d0ceb5c5b06da5df
Alquipo <alquiponeto@outlook.com.br>
2021-01-23 21:56:20 -0300

    fix: test checkboc

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -11,4 +11,10 @@ describe('<Checkbox />', () => {
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
   })
+
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
 })
