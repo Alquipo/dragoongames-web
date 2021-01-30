1fc6c7e30532909e6e3e56418eaec625e2a699e7
Alquipo <alquiponeto@outlook.com.br>
2021-01-30 20:00:52 -0300

    feat: GamesPage

---
+++ src/templates/Games/index.tsx
@@ -0,0 +1,39 @@
+import Base from 'templates/Base'
+import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
+
+import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import { Grid } from 'components/Grid'
+
+import * as S from './styles'
+
+export type GamesTemplateProps = {
+  games?: GameCardProps[]
+  filterItems: ItemProps[]
+}
+
+const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => (
+  <Base>
+    <S.Main>
+      <ExploreSidebar
+        items={filterItems}
+        onFilter={() => console.log('filter')}
+      />
+
+      <section>
+        <Grid>
+          {games.map((item) => (
+            <GameCard key={item.title} {...item} />
+          ))}
+        </Grid>
+
+        <S.ShowMore role="button" onClick={() => console.log('show more')}>
+          <p>Show More</p>
+          <ArrowDown size={35} />
+        </S.ShowMore>
+      </section>
+    </S.Main>
+  </Base>
+)
+
+export default GamesTemplate
