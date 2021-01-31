fd9b73c0f30eda65870bdd214a1ce2fcf637a5cf
Alquipo <alquiponeto@outlook.com.br>
2021-01-31 01:23:33 -0300

    fix: query apollo gamespage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import { useApollo } from 'utils/apollo'
 
 function App({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
-    cache: new InMemoryCache()
-  })
+  const client = useApollo(pageProps.initialApolloState)
 
   return (
     <ApolloProvider client={client}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,12 @@ import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
-// import { gql, useQuery } from '@apollo/client'
 
 export default function Index(props: HomeTemplateProps) {
-  // const { data, loading, error } = useQuery(gql`
-  //   query getGames {
-  //     games {
-  //       name
-  //     }
-  //   }
-  // `)
-
-  // if (loading) return <p>Loading...</p>
-
-  // if (error) return <p>{error}</p>
-
-  // if (data) return <p>{JSON.stringify(data, null, 2)}</p>
-
   return <Home {...props} />
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
   return {
     props: {
       banners: bannersMock,
