55b7d3bed0d767132cc04b140c8273db9978e73d
Alquipo <alquiponeto@outlook.com.br>
2021-01-30 23:21:58 -0300

    feat: apollo provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,23 @@ import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+// import { gql, useQuery } from '@apollo/client'
 
 export default function Index(props: HomeTemplateProps) {
+  // const { data, loading, error } = useQuery(gql`
+  //   query getGames {
+  //     games {
+  //       name
+  //     }
+  //   }
+  // `)
+
+  // if (loading) return <p>Loading...</p>
+
+  // if (error) return <p>{error}</p>
+
+  // if (data) return <p>{JSON.stringify(data, null, 2)}</p>
+
   return <Home {...props} />
 }
 
