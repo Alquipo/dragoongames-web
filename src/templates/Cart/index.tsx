eedd01a0bdf725b960c3810b696d73df145e6ca7
Alquipo <alquiponeto@outlook.com.br>
2021-01-30 21:54:15 -0300

    fix: cartDropdown component

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -33,7 +33,7 @@ const Cart = ({
           My Cart
         </Heading>
 
-        {items.length ? (
+        {items?.length ? (
           <S.Content>
             <CartList items={items} total={total} />
             <PaymentOptions cards={cards} handlePayment={handlePayment} />
