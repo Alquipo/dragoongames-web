5e962567819bb8e422599753270077f175365084
Alquipo <alquiponeto@outlook.com.br>
2021-01-31 18:43:17 -0300

    Fix: api in home page newgames

---
+++ src/graphql/fragments/highlight.ts
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client'
+
+export const HighlightFragment = gql`
+  fragment HighlightFragment on ComponentPageHighlight {
+    title
+    subtitle
+    background {
+      url
+    }
+    floatImage {
+      url
+    }
+
+    buttonLabel
+    buttonLink
+    alignment
+  }
+`
