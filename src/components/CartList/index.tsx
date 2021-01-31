ae9afc03ebe2fb4e0afdca6cb31dfa0ea40a668a
Alquipo <alquiponeto@outlook.com.br>
2021-01-30 21:50:42 -0300

    fix: cartlist component

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,31 +1,42 @@
 import Button from 'components/Button'
+import Empty from 'components/Empty'
 import GameItem, { GameItemProps } from 'components/GameItem'
 import Link from 'next/link'
 
 import * as S from './styles'
 
 export type CartListProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
   hasButton?: boolean
 }
 
-const CartList = ({ items, total, hasButton = false }: CartListProps) => (
-  <S.Wrapper>
-    {items.map((item) => (
-      <GameItem key={item.title} {...item} />
-    ))}
+const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
+  <S.Wrapper isEmpty={!items.length}>
+    {items.length ? (
+      <>
+        {items.map((item) => (
+          <GameItem key={item.title} {...item} />
+        ))}
 
-    <S.Footer>
-      {!hasButton && <span>Total:</span>}
-      <S.Total>{total}</S.Total>
+        <S.Footer>
+          {!hasButton && <span>Total:</span>}
+          <S.Total>{total}</S.Total>
 
-      {hasButton && (
-        <Link href="/cart">
-          <Button as="a">Buy it now</Button>
-        </Link>
-      )}
-    </S.Footer>
+          {hasButton && (
+            <Link href="/cart">
+              <Button as="a">Buy it now</Button>
+            </Link>
+          )}
+        </S.Footer>
+      </>
+    ) : (
+      <Empty
+        title="Your cart is empty"
+        description="Go back to the store and explore great games and offers"
+        hasLink
+      />
+    )}
   </S.Wrapper>
 )
 
