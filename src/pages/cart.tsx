3aeb5c99a39e12fb87b905cb82eec4dffb91f67c
Alquipo <alquiponeto@outlook.com.br>
2021-02-06 16:23:10 -0300

    feat: cartpage

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,33 @@
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+
 import Cart, { CartProps } from 'templates/Cart'
 
 import itemsMock from 'components/CartList/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
 import cardsMock from 'components/PaymentOptions/mock'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
       items: itemsMock,
       total: '$ 430,00',
       cards: cardsMock,
-      recommendedGames: gamesMock,
-      recommendedHighlight: highlightMock
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      )
     }
   }
 }
