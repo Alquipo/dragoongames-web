bc19c01aaa86cd820e8dcb23f5dee1df5161c00f
Alquipo <alquiponeto@outlook.com.br>
2021-03-05 15:07:00 -0300

    fix: query filter

--- src/graphql/generated/QueryGames.ts
+++ src/graphql/generated/QueryGames.ts
@@ -8,29 +8,31 @@
 // ====================================================
 
 export interface QueryGames_games_cover {
-  __typename: "UploadFile";
-  url: string;
+  __typename: 'UploadFile'
+  url: string
 }
 
 export interface QueryGames_games_developers {
-  __typename: "Developer";
-  name: string;
+  __typename: 'Developer'
+  name: string
 }
 
 export interface QueryGames_games {
-  __typename: "Game";
-  name: string;
-  slug: string;
-  cover: QueryGames_games_cover | null;
-  developers: QueryGames_games_developers[];
-  price: number;
+  __typename: 'Game'
+  name: string
+  slug: string
+  cover: QueryGames_games_cover | null
+  developers: QueryGames_games_developers[]
+  price: number
 }
 
 export interface QueryGames {
-  games: QueryGames_games[];
+  games: QueryGames_games[]
 }
 
 export interface QueryGamesVariables {
-  limit: number;
-  start?: number | null;
+  limit: number
+  start?: number | null
+  where?: any | null
+  sort?: string | null
 }
