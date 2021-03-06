193a8183ec54fbb50b2600cd3c96afc07954cea6
Alquipo <alquiponeto@outlook.com.br>
2021-03-06 16:15:12 -0300

    fix: GameButton component

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -10,14 +10,17 @@ import Heading from 'components/Heading'
 import Ribbon from 'components/Ribbon'
 
 import * as S from './styles'
+import React from 'react'
+import CartButton from 'components/CartButton'
 
 export type GameInfoProps = {
+  id: string
   title: string
   description: string
   price: number
 }
 
-const GameInfo = ({ title, description, price }: GameInfoProps) => (
+const GameInfo = ({ id, title, description, price }: GameInfoProps) => (
   <S.Wrapper>
     <Heading color="black" lineBottom>
       {title}
@@ -28,9 +31,7 @@ const GameInfo = ({ title, description, price }: GameInfoProps) => (
     <S.Description>{description}</S.Description>
 
     <S.ButtonsWrapper>
-      <Button icon={<AddShoppingCart />} size="large">
-        Add to cart
-      </Button>
+      <CartButton id={id} size="large" hasText />
       <Button icon={<FavoriteBorder />} size="large" minimal>
         Wishlist
       </Button>
