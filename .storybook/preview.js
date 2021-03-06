a9a2f60d7132b03efaff6810b4d2903a87acbd16
Alquipo <alquiponeto@outlook.com.br>
2021-03-06 14:59:45 -0300

    fix: Storybook CartProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { addDecorator } from '@storybook/react'
 import { withNextRouter } from 'storybook-addon-next-router'
+import { CartContext, CartContextDefaultValues } from 'hooks/use-cart'
 
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
@@ -24,10 +25,18 @@ export const parameters = {
 }
 
 export const decorators = [
-  (Story) => (
+  (Story, context) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles removeBg />
-      <Story />
+      <CartContext.Provider
+        value={{
+          ...CartContextDefaultValues,
+          ...(context?.args?.cartContextValue || {}),
+          ...context.args
+        }}
+      >
+        <GlobalStyles removeBg />
+        <Story />
+      </CartContext.Provider>
     </ThemeProvider>
   )
 ]
