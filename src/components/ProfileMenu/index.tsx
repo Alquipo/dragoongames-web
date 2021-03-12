b1db98ff11cb176ca104d6c35e2979259b75dcd5
Alquipo <alquiponeto@outlook.com.br>
2021-03-12 14:59:00 -0300

    fix: signOut button

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { signOut } from 'next-auth/client'
 import {
   AccountCircle,
   CreditCard,
@@ -34,12 +35,10 @@ const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
       </S.Link>
     </Link>
 
-    <Link href="/logout" passHref>
-      <S.Link>
-        <ExitToApp size={24} />
-        <span>Sign out</span>
-      </S.Link>
-    </Link>
+    <S.Link role="button" onClick={() => signOut()}>
+      <ExitToApp size={24} />
+      <span>Sign out</span>
+    </S.Link>
   </S.Nav>
 )
 
