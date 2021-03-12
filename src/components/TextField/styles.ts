01fb52dd3b5ca70c64d274030f00a589589633ff
Alquipo <alquiponeto@outlook.com.br>
2021-03-12 12:02:51 -0300

    fix: autofill

--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -32,6 +32,11 @@ export const Input = styled.input<IconPositionProps>`
     border: 0;
     outline: none;
     width: 100%;
+
+    &:-webkit-autofill{
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small} ${theme.colors.lightGray} inset;
+      filter: none;
+    }
   `}
 `
 
