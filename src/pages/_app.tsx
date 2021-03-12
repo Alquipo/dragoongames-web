2b7de49105ee1791096c0acfa62bc16e4f686690
Alquipo <alquiponeto@outlook.com.br>
2021-03-12 11:58:47 -0300

    chore: nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import NextNprogress from 'nextjs-progressbar'
 import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 import { CartProvider } from 'hooks/use-cart'
+
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
@@ -26,6 +28,12 @@ function App({ Component, pageProps }: AppProps) {
             />
           </Head>
           <GlobalStyles />
+          <NextNprogress
+            color="#F231A5"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={4}
+          />
           <Component {...pageProps} />
         </CartProvider>
       </ThemeProvider>
