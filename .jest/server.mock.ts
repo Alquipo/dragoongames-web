31f1cc12cfc9864e3eefcaa3baff2cef25270fd7
Alquipo <alquiponeto@outlook.com.br>
2021-05-26 18:38:04 -0300

    feat: msw config

---
+++ .jest/server.mock.ts
@@ -0,0 +1,16 @@
+import { server } from '../src/utils/mockServer/server'
+
+beforeAll(() => {
+  //ficar escutando todas as chamadas nos teste
+  server.listen()
+})
+
+afterEach(() => {
+  //reseta todos os handlers para caso eles sejam chamados novamente
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  //fecha o serve e limpa tudo
+  server.close
+})
