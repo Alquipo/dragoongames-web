ea4abb86bad0e2d02d0a04beb1134e5a329b785b
Alquipo <alquiponeto@outlook.com.br>
2021-05-27 14:37:55 -0300

    feat: stripe integration

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,3 +1,6 @@
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
 import CartList, { CartListProps } from 'components/CartList'
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
@@ -17,6 +20,8 @@ export type CartProps = {
   recommendedHighlight: HighlightProps
 } & CartListProps
 
+const stripe = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
+
 const Cart = ({
   recommendedGames,
   recommendedTitle,
@@ -31,7 +36,10 @@ const Cart = ({
 
         <S.Content>
           <CartList />
-          <PaymentForm />
+
+          <Elements stripe={stripe}>
+            <PaymentForm />
+          </Elements>
         </S.Content>
 
         <S.Text>
