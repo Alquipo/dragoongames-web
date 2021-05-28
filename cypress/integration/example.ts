8ec9213dd9738d816c36a974192216697ca64078
Alquipo <alquiponeto@outlook.com.br>
2021-05-28 20:29:29 -0300

    fix: eslint cong cypress

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,8 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should go to google', () => {
+  it.skip('should go to google', () => {
     cy.google()
+
   })
+
+  it('should change light/dark theme on willian justen site', () => {
+    cy.visit('https://willianjusten.com.br')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.light').should('exist')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.dark').should('exist')
+  })
+
+
 })
