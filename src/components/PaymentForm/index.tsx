710dabc3e6508cba565b4cd65fb4ecefdd652cc9
Alquipo <alquiponeto@outlook.com.br>
2021-05-28 09:30:58 -0300

    fix: save Order cart in BD

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -2,11 +2,11 @@ import { Session } from 'next-auth'
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+import { PaymentIntent, StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
 
 import { useCart } from 'hooks/use-cart'
-import { createPaymentIntent } from 'utils/stripe/methods'
+import { createPayment, createPaymentIntent } from 'utils/stripe/methods'
 
 import Button from 'components/Button'
 import { FormLoading } from 'components/Form'
@@ -66,11 +66,22 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     setError(event.error ? event.error.message : '')
   }
 
+  const saveOrder = async (paymentIntent?: PaymentIntent) => {
+    const data = await createPayment({
+      items,
+      paymentIntent,
+      token: session.jwt as string
+    })
+
+    return data
+  }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
     setLoading(true)
 
     if (freeGames) {
+      saveOrder()
       push('/success')
       return
     }
@@ -89,6 +100,8 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
       setLoading(false)
 
       // salvar a compra no banco do Strapi
+
+      saveOrder(payload.paymentIntent)
       // redirectionar para a página de Sucesso
       push('/success')
     }
--- src/utils/stripe/methods.tsx
+++ src/utils/stripe/methods.tsx
@@ -1,25 +1,56 @@
+import { PaymentIntent } from '@stripe/stripe-js'
 import { CartItem } from 'hooks/use-cart'
+
+type FetcherParams = {
+  url: string
+  body: string
+  token: string
+}
+
+const fetcher = async ({ body, token, url }: FetcherParams) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    body
+  })
+
+  return await response.json()
+}
 
 type PaymentIntentParams = {
   items: CartItem[]
   token: string
 }
 
-export const createPaymentIntent = async ({
-  items,
-  token
-}: PaymentIntentParams) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/orders/create-payment-intent`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ cart: items })
-    }
-  )
+export const createPaymentIntent = ({ items, token }: PaymentIntentParams) => {
+  return fetcher({
+    url: '/orders/create-payment-intent',
+    body: JSON.stringify({ cart: items }),
+    token
+  })
+}
 
-  return await response.json()
+type CreatePaymentParams = {
+  items: CartItem[]
+  paymentIntent?: PaymentIntent
+  token: string
+}
+
+export const createPayment = ({
+  items,
+  token,
+  paymentIntent
+}: CreatePaymentParams) => {
+  return fetcher({
+    url: '/orders',
+    body: JSON.stringify({
+      cart: items,
+      paymentIntentId: paymentIntent?.id,
+      paymentMethod: paymentIntent?.payment_method
+    }),
+    token
+  })
 }
