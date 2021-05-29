757bbbf42533a89fe2dab7ca6f35a88561c3e892
Alquipo <alquiponeto@outlook.com.br>
2021-05-29 18:02:17 -0300

    feat: finished test e2e wishlist page

--- src/templates/WishList/index.tsx
+++ src/templates/WishList/index.tsx
@@ -29,7 +29,7 @@ const WishList = ({
 
   return (
     <Base>
-      <Container>
+      <Container data-cy="wishlist">
         <Heading lineLeft lineColor="secondary">
           Wishlist
         </Heading>
