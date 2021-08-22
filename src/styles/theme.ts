cca5375ef5784cbad4c30e6ca51b386e0e59803b
Alquipo <alquiponeto@outlook.com.br>
2021-08-22 19:43:39 -0300

    fix: colors secondary

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -24,14 +24,13 @@ export default {
   },
   colors: {
     primary: '#CD113B',
-    secondary: '#faa307',
+    secondary: '#FF6347',
     mainBg: '#03071e',
     lightBg: '#F2F2F2',
     white: '#FAFAFA',
     black: '#030517',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
-    darkGray: '#2E2F42',
     red: '#FF6347'
   },
   spacings: {
